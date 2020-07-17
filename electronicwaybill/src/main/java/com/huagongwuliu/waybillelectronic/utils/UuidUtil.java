da0a63f10ce1894b7f5fb12df5c0d9d38468f90f
merlin <zy44638@163.com>
2020-07-17 15:05:27 +0800

    [feature]一些utils

---
+++ electronicwaybill/src/main/java/com/huagongwuliu/waybillelectronic/utils/UuidUtil.java
@@ -0,0 +1,14 @@
+package com.huagongwuliu.waybillelectronic.utils;
+
+import java.util.UUID;
+
+/**
+ * uuid 工具类，用于获取32位不重复字符串
+ */
+public class UuidUtil {
+
+    public static String getUUID (){
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().replaceAll("-", "");
+    }
+}
